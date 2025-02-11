f45f6a397c9c078f8fb08729c77c60e7b4607194
Prashidha Poudel <prashidha45@gmail.com>
2025-02-10 22:50:27 -0600

    Commit

--- Backend/server.js
+++ Backend/server.js
@@ -6,7 +6,7 @@ import { Server } from "socket.io"
 const app = express()
 app.use(cors())
 
-const io = new Server({origin: "https://guffgaff-4y84.onrender.com/",cors:true})
+const io = new Server({origin: "https://guffandgaff.netlify.app/",cors:true})
 
 app.listen(4000, () => {
     console.log("Server running on port 4000")
